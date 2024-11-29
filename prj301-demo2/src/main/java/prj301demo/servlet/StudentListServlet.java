f14270c876c737e1c48b4fc88cefb15ec509ff56
datnt171874 <danielsmith2511@gmail.com>
2024-11-29 13:30:56 +0700

    Update StudentListServlet.java

--- prj301-demo2/src/main/java/prj301demo/servlet/StudentListServlet.java
+++ prj301-demo2/src/main/java/prj301demo/servlet/StudentListServlet.java
@@ -24,31 +24,63 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class StudentListServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet StudentListServlet</title>");            
+            out.println("<title>Servlet StudentListServlet</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Student List </h1>");
-            
-           
-            
-        
+
+            String keyword = request.getParameter("keyword");
+
+            out.println("<form action=''>\n"
+                    + " <input name=keyword type=text>\n"
+                    + " <input type=submit value=Search>\n"
+                    + " </form>");
+            out.println("<table>");
+            out.print("<tr><td>Id</td>");
+            out.print("<td>First Name</td>");
+            out.print("<td>Last Name</td>");
+            out.print("<td>Age</td></tr>");
+            try {
+                Connection cnn = DBUtils.getConnection();
+
+                String sql = "select id, firstname, lastname, age from student";
+
+                if (keyword != null && !keyword.isEmpty()) {
+                    sql += "where lastname like ? OR firstname like ?";
+                }
+                PreparedStatement pst = cnn.prepareStatement(sql);
+
+                if (keyword != null && !keyword.isEmpty()) {
+                    pst.setString(1, "%" + keyword + "%");
+                    pst.setString(2, "%" + keyword + "%");
+                }
+                ResultSet rs = pst.executeQuery();
+                if (rs != null) {
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        String firstname = rs.getString("firstname");
+                        String lastname = rs.getString("lastname");
+                        int age = rs.getInt("age");
+
+                        out.print("<tr><td>" + id + "</td>");
+                        out.print("<td>" + firstname + "</td>");
+                        out.print("<td>" + lastname + "</td>");
+                        out.print("<td>" + age + "</td></tr>");
+                    }
+                }
+                cnn.close();
+            } catch (SQLException ex) {
+                System.out.println("error in sql " + ex.getMessage());
+                ex.printStackTrace();
+            }
+
             out.println("</table>");
             out.println("</body>");
             out.println("</html>");
